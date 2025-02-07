09258402135cf6ea3634b12e0866517355722608
hkeidt <hkeidt@gmail.com>
2025-02-07 13:12:44 -0300

    Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -1,3 +1,7 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
 # Configuração do Streamlit
 st.set_page_config(page_title='Dashboard de Apostas', layout='wide')
 st.title('🎲 SBIntelligence - Dashboard Profit')
